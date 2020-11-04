bd39e64de0bf818def8af910fde796201a7fb758
vicknesh <vicknesh@Ubuntu20.04>
2020-11-04 16:41:06 -0700

    Added Absolute scale

--- main.py
+++ main.py
@@ -4,239 +4,245 @@ import glob
 import os
 import matplotlib.pyplot as plt
 from icp import icp
+import sys
 
 class Stereo:
     
-    IMG_L = []
-    IMG_R = []
+    img_L = []
+    img_R = []
     kp_l = []
     kp_r = []
     des_l = []
     des_r = []
+    points3D = []
     
     def __init__(self, frameNo):
         self.frameNo = frameNo
     
-    def Matching(self, IMG_L, IMG_R):
-        self.IMG_L = IMG_L
-        self.IMG_R = IMG_R
-        self.kp_l, self.des_l = KpDes(IMG_L)
-        self.kp_r, self.des_r = KpDes(IMG_R)
+    def Matching(self, img_L, img_R):
+        self.img_L = img_L
+        self.img_R = img_R
+        self.kp_l, self.des_l = self.KpDes(self.img_L)
+        self.kp_r, self.des_r = self.KpDes(self.img_R)
+        
+    def KpDes(self, IMG):
+        # Initiate ORB detector
+        orb = cv2.ORB_create()
+        # Initiate FAST detector
+        fast = cv2.FastFeatureDetector_create(threshold=50, nonmaxSuppression=True)
+        # find and draw the keypoints
+        kp = fast.detect(IMG, None)
+        
+        # find the keypoints and compute the descriptors with ORB
+        kp, des = orb.compute(IMG, kp)
+        
+        return kp, des
     
 
 def main():
     
+    try:
+        fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+        fname = "DepthImage.avi"
+        fps = 15.0
+        videoWriter = cv2.VideoWriter(fname, fourcc, fps, (1241, 376))
+    except:
+        print("Error: can't create output video: %s" % fname)
+        sys.exit()
     
-    maxMatches = 50
+    maxMatches = 500
     frameNo = 0
     
-    currentTransformation = np.eye(4)
+    currentT_L = np.eye(4)
+    currentT_R = np.eye(4)
     
-    mapImage = np.zeros((1024,1024))
+    odom = np.zeros((1024,1024,3))
+    kpR = []
+    datasetDir = "dataset/sequences/"
     
-    datasetDir = "dataset/sequences/*"
-    for sequence in glob.glob(datasetDir):
+    sequence = "00"   
+    calibFile = open(datasetDir + sequence + "/calib.txt","r")
+    timeFile = open("dataset/poses/" + sequence + ".txt","r")
+    projectionMatrices = calibFile.read().split()
+    
+    focalLength = float(projectionMatrices[1])
+    baseLength =  -1*float(projectionMatrices[17])/float(projectionMatrices[14]) # base = -P1(1,4)/P1(1,1) (in meters)
+    
+    K = [[float(projectionMatrices[1]), float(projectionMatrices[2]), float(projectionMatrices[3])],
+                       [float(projectionMatrices[5]), float(projectionMatrices[6]), float(projectionMatrices[7])],
+                       [float(projectionMatrices[9]), float(projectionMatrices[10]), float(projectionMatrices[11])]]
+    
+    cx = float(projectionMatrices[3])
+    cy = float(projectionMatrices[7])
+    t = timeFile.readlines()
+    #print(t)
+    
+    frameNo = 0
+    imageNames = os.listdir(datasetDir + sequence + "/image_0")
+    imageNames.sort()
+    for image in imageNames:
         
-        calibFile = open(sequence + "/calib.txt","r")
-        #print(calibFile.read().split()[17])#, calibFile.read().split()[15], calibFile.read().split()[18])
-        projectionMatrices = calibFile.read().split()
+        current = Stereo(frameNo)
         
-        focalLength = float(projectionMatrices[1])
-        baseLength =  -1*float(projectionMatrices[17])/float(projectionMatrices[14]) # base = -P1(1,4)/P1(1,1) (in meters)
+        imgL = cv2.imread(datasetDir + sequence + "/image_0/" + image)
+        imgR = cv2.imread(datasetDir + sequence + "/image_1/" + image)
         
-        intrinsicMatrix = [[float(projectionMatrices[1]), float(projectionMatrices[2]), float(projectionMatrices[3])],
-                           [float(projectionMatrices[5]), float(projectionMatrices[6]), float(projectionMatrices[7])],
-                           [float(projectionMatrices[9]), float(projectionMatrices[10]), float(projectionMatrices[11])]]
+        imgL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+        imgR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
+        current.Matching(imgL, imgR)
         
-        cx = float(projectionMatrices[3])
-        cy = float(projectionMatrices[7])
+        scale, x, y, z = getAbsoluteScale(frameNo, t)
+        print("scale = ", scale)
         
-        #print(intrinsicMatrix)
-        #print(focalLength, baseLength)
+        if frameNo:
+            currentT_L, currentT_R, kpL, matchesL, matchesR = poseEstimation(previous, current, focalLength, baseLength , cx , cy, currentT_L, currentT_R, K, scale)
+            
+        print(currentT_L[:3,3])
         
-        imageNames = os.listdir(sequence + "/image_0")
-        imageNames.sort()
-        for image in imageNames:
-            current = Stereo(frameNo)
+        odom = cv2.circle(odom, (int(currentT_L[0,3]) + 512, int(currentT_L[2,3]) + 512), 2, (255,0,0), 2)
+        odom = cv2.circle(odom, (int(currentT_R[0,3]) + 512, int(currentT_R[2,3]) + 512), 2, (0,0,255), 2)
+        odom = cv2.circle(odom, (int(x) + 512, int(z) + 512), 2, (0,255,0), 2)
+        
+        
+        previous = current
+        
+        
+        if frameNo:
+            for i in range(len(current.points3D)):
+                current.img_L = cv2.circle(current.img_L, tuple(matchesL[i]), 2, (255,0,0), 1)
+                current.img_L = cv2.putText(current.img_L, str(round(current.points3D[i][2], 2)), (matchesL[i][0] + 5, matchesL[i][1] + 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 1, cv2.LINE_AA)
             
-            imgL = cv2.imread(sequence + "/image_0/" + image)
-            imgR = cv2.imread(sequence + "/image_1/" + image)
-            
-            #h, w, _ = imgL.shape
-            #print(h, w)
-            
-            current.Matching(imgL, imgR)
-            
-            #kp_l, des_l = KpDes(IMG_L)
-            #kp_r, des_r = KpDes(IMG_R)
-            
-            #print(current.kp_l)
-            
-            #IMG_L1 = cv2.drawKeypoints(IMG_L, kp_l, None, color = (0,255,0), flags = 0)
-            #IMG_R1 = cv2.drawKeypoints(IMG_R, kp_r, None, color = (0,255,0), flags = 0)
-            
-            
-            if frameNo:
-                currentTransformation = poseEstimation(previous, current, maxMatches, focalLength, baseLength , cx , cy, currentTransformation)
-                
-            print(currentTransformation[:3,3])
-            
-            #mapImage[int(currentTransformation[2,3]) + 512, int(currentTransformation[0,3]) + 512] = 255
-            
-            mapImage = cv2.circle(mapImage, (int(currentTransformation[0,3]) + 512, int(currentTransformation[2,3]) + 512), 2, (255,0,0), 2)
-               
-            previous = current
-            
-            #previous3Dpoints = current3Dpoints
-            
-            #print(current3Dpoints)
-            
-            '''for i in range(len(bestMatches_Train)):
-                IMG_L = cv2.circle(IMG_L, (bestMatches_Train[i]), 2, (255,0,0), 1)
-                IMG_L = cv2.putText(IMG_L, str(current3Dpoints[i][2]), (bestMatches_Train[i][0] + 5, bestMatches_Train[i][1] + 5), cv2.FONT_HERSHEY_SIMPLEX, .3, (0,0,255), 1, cv2.LINE_AA)'''
-            
-            frameNo = frameNo + 1
-            cv2.imshow("Left Image", current.IMG_L)
-            # cv2.imshow("Right Image", IMG_R)
-            # cv2.imshow("Match Image", matchIMG)
-            # cv2.waitKey(0)
-            cv2.imshow("Map", mapImage)
-            key_pressed = cv2.waitKey(1) & 0xFF
-            
-            if key_pressed == ord('q') or key_pressed == 27:
-                break
+            cv2.imshow("Left Image", current.img_L)
+            videoWriter.write(current.img_L)
+        
+        frameNo = frameNo + 1
+        cv2.imshow("Map", odom)
+        key_pressed = cv2.waitKey(1) & 0xFF
+        
         if key_pressed == ord('q') or key_pressed == 27:
-                break
-
-def KpDes(IMG):
-        
-    # Initiate ORB detector
-    orb = cv2.ORB_create()
-    # find the keypoints and compute the descriptors with ORB
-    kp, des = orb.detectAndCompute(IMG,None)
-    
-    return kp, des
-
-def DesMatch(IMG_L, kp_l, des_l, IMG_R, kp_r, des_r, maxMatches = 100, sort = True):
-    # create BFMatcher object
-    bfObj = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    # Match descriptors.
-    #print(type(des_l))
-    matches = bfObj.match(des_l,des_r)
-    # Sort them in the order of their distance.
-    if sort:
-        matches = sorted(matches, key = lambda x:x.distance)
-    
-    bestMatches_Train = []
-    bestMatches_Query = []
-    descriptor_Train = []
-    descriptor_Query = []
-    keypoints_Train = []
-    keypoints_Query = []
-    
-    for match in matches:
-        #print(match)
-        #print(kp_l[match.trainIdx], kp_r[match.queryIdx])
-        bestMatches_Query.append((int(kp_l[match.queryIdx].pt[0]), int(kp_l[match.queryIdx].pt[1])))
-        bestMatches_Train.append((int(kp_r[match.trainIdx].pt[0]), int(kp_r[match.trainIdx].pt[1])))
-        keypoints_Query.append(kp_l[match.queryIdx])
-        keypoints_Train.append(kp_r[match.trainIdx])
-        descriptor_Query.append(des_l[match.queryIdx])
-        descriptor_Train.append(des_r[match.trainIdx])
-        if len(bestMatches_Query) == maxMatches:
             break
-        
-    #print(bestMatches_Train[0][0], bestMatches_Query[0][0])
-    #keypoints_Train = np.array(bestMatches_Train)
-    #keypoints_Query = np.array(bestMatches_Query)
-    descriptor_Train = np.array(descriptor_Train)
-    descriptor_Query = np.array(descriptor_Query)
-    
-    #print(type(keypoints_Train), type(keypoints_Query[0]))
-    #print(type(descriptor_Train), type(descriptor_Query[0]))
-    
-    # Draw first 100 matches.
-    #IMG = cv2.drawMatches(IMG_L, kp_l, IMG_R, kp_r, matches[:maxMatches], None, flags = cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    #cv2.imshow("Match Image", IMG)
-    #cv2.waitKey(0)
-    
-    return bestMatches_Train, bestMatches_Query, descriptor_Train, descriptor_Query, keypoints_Train, keypoints_Query
 
 
-def findWorldPts(bestMatches_Train, bestMatches_Query, focalLength, baseLength, cx, cy):
+
+def DesMatch(img_L, kp_l, des_l, img_R, kp_r, des_r, sort = True):
+    # create BFMatcher object
+    bfObj = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+    # Match descriptors.
+    matches = bfObj.match(des_l,des_r)
+    
+    kpL = []
+    kpR = []
+    
+    minDist = 10000
+    maxDist = 0
+    
+    for i in range(len(matches)):
+        dist = matches[i].distance
+        if dist < minDist:
+            minDist = dist
+        if dist > maxDist:
+            maxDist = dist
+    
+    good = []
+    
+    for i in range(len(matches)):
+        if matches[i].distance <= max(2 * minDist, 30):
+            kpL.append((int(kp_l[matches[i].queryIdx].pt[0]), int(kp_l[matches[i].queryIdx].pt[1])))
+            kpR.append((int(kp_r[matches[i].trainIdx].pt[0]), int(kp_r[matches[i].trainIdx].pt[1])))
+            good.append(matches[i])
+            
+    kpR = np.array(kpR)
+    kpL = np.array(kpL)
+    
+    return kpR, kpL, good
+
+
+def findWorldPts(kpR, kpL, focalLength, baseLength, cx, cy):
 
     
-    current3Dpoints = []
-    for i in range(len(bestMatches_Train)):
+    points3D = []
+    matchesL = []
+    matchesR = []
+    for i in range(len(kpR)):
         
-        d = bestMatches_Query[i][0] - bestMatches_Train[i][0]
+        d = kpL[i][0] - kpR[i][0]
         
-        parallelCheck = bestMatches_Query[i][1] - bestMatches_Train[i][1]
+        parallelCheck = kpL[i][1] - kpR[i][1]
         
-        if (d > 0 && abs(parallelCheck) < 10):
+        
+        if (d > 0 and abs(parallelCheck) < 10):
             calcZ = (focalLength * baseLength) / d # Z = f*b/x1-x2
             Z = calcZ
+            matchesL.append(kpL[i])
+            matchesR.append(kpR[i])
+            #print(kpR[i][0], kpL[i][0])
             
-            #print(bestMatches_Train[i][0], bestMatches_Query[i][0])
+            X = np.abs(kpL[i][0]-cx) * calcZ / focalLength # X = x1*Z/f
+            Y = np.abs(kpL[i][1]-cy) * calcZ / focalLength # Y = y1*Z/f
             
-            X = np.abs(bestMatches_Query[i][0]-cx) * calcZ / focalLength # X = x1*Z/f
-            Y = np.abs(bestMatches_Query[i][1]-cy) * calcZ / focalLength # Y = y1*Z/f
-            
-            current3Dpoints.append([X, Y, Z])
+            if (Z > 5):
+                points3D.append([X, Y, Z])
         
-    return np.array(current3Dpoints)
+    return np.array(points3D), matchesL, matchesR
 
-def poseEstimation(previous, current, maxMatches, focalLength, baseLength , cx , cy, currentTransformation):
+def poseEstimation(previous, current, focalLength, baseLength , cx , cy, prevT_L, prevT_R, K, scale):
     
-    bestMatches_Train, bestMatches_Query, descriptor_Train, descriptor_Query, keypoints_Train, keypoints_Query = DesMatch(previous.IMG_L, previous.kp_l, previous.des_l, previous.IMG_R, previous.kp_r, previous.des_r, maxMatches)
+    kpR, kpL, good = DesMatch(previous.img_L, previous.kp_l, previous.des_l, previous.img_R, previous.kp_r, previous.des_r)
             
-    previous3Dpoints = findWorldPts(bestMatches_Train, bestMatches_Query, focalLength, baseLength , cx , cy)
+    previous.points3D, matchesL, matchesR = findWorldPts(kpR, kpL, focalLength, baseLength , cx , cy)
     
-    #bestMatches_Train, bestMatches_Query, descriptor_Train, descriptor_Query, keypoints_Train, keypoints_Query = DesMatch(previous.IMG_L, keypoints_Query, descriptor_Query, current.IMG_L, current.kp_l, current.des_l, maxMatches)
+    kpR, kpL, good = DesMatch(previous.img_L, previous.kp_l, previous.des_l, current.img_L, current.kp_l, current.des_l)
     
-    bestMatches_Train, bestMatches_Query, descriptor_Train, descriptor_Query, keypoints_Train, keypoints_Query = DesMatch(current.IMG_L, keypoints_Train, descriptor_Train, current.IMG_R, current.kp_r, current.des_r)
     
-    #print("QUERY\n", bestMatches_Query)
-    #print("TRAIN\n", bestMatches_Train)
+    E, mask = cv2.findEssentialMat(kpR, kpL, focalLength, (cx, cy), method=cv2.RANSAC, prob=0.999, threshold=1.0)
     
-    current3Dpoints = findWorldPts(bestMatches_Train, bestMatches_Query, focalLength, baseLength , cx , cy)
+    _, R, t, mask = cv2.recoverPose(E, kpR, kpL, focal=focalLength, pp = (cx,cy))
     
-    #print(previous3Dpoints.shape)
-    #print(current3Dpoints.shape)
+    kpR, kpL, good = DesMatch(previous.img_R, previous.kp_r, previous.des_r, current.img_R, current.kp_r, current.des_r)
     
-    #H = fitPoint(previous3Dpoints, current3Dpoints)
-    T, distances, i = icp(previous3Dpoints, current3Dpoints)
     
-    currentTransformation = currentTransformation @ T
+    R_prev = prevT_L[0:3, 0:3]
+    t_prev = np.reshape(prevT_L[0:3, 3], (3,1))
     
-    return currentTransformation
+    R_curr = R @ R_prev
+    t_curr = t_prev + scale*(R_prev @ t)
+    
+    
+    currentT_L = np.block([[R_curr, t_curr],[0, 0, 0, 1]])
+    
+    E, mask = cv2.findEssentialMat(kpR, kpL, focalLength, (cx, cy), method=cv2.RANSAC, prob=0.999, threshold=1.0)
+    
+    _, R, t, mask = cv2.recoverPose(E, kpR, kpL, focal=focalLength, pp = (cx,cy))
+    
+    
+    kpR, kpL, good = DesMatch(current.img_L, current.kp_l, current.des_l, current.img_R, current.kp_r, current.des_r)
+    
+    
+    current.points3D, matchesL, matchesR = findWorldPts(kpR, kpL, focalLength, baseLength , cx , cy)
+    
+    R_prev = prevT_R[0:3, 0:3]
+    t_prev = np.reshape(prevT_R[0:3, 3], (3,1))
+    
+    R_curr = R @ R_prev
+    t_curr = t_prev + scale*(R_prev @ t)
+    
+    
+    currentT_R = np.block([[R_curr, t_curr],[0, 0, 0, 1]])
+    
+    return currentT_L, currentT_R, kpL, matchesL, matchesR
 
-def fitPoint(previous3Dpoints, current3Dpoints):
     
-    centroidPrevious = np.mean(previous3Dpoints, axis = 0)
-    centroidCurrent = np.mean(current3Dpoints, axis = 0)
     
-    Previous = previous3Dpoints - centroidPrevious
-    Current = current3Dpoints - centroidCurrent
-    
-    W = np.dot(Previous.T, Current)
-    
-    U, S, Vt = np.linalg.svd(W)
-    
-    R = np.dot(Vt.T, U.T)
-    
-    if np.linalg.det(R) < 0:
-        Vt[2, :] *= -1
-        R = np.dot(Vt.T, U.T)
-        
-    t = centroidCurrent.T - np.dot(R, centroidPrevious.T)
-    
-    H = np.eye(4)
-    H[:3,:3] = R
-    H[:3,3] = t
-    #print(H)
-    
-    return H
+def getAbsoluteScale(frameNo, t):
+    ss = t[frameNo-1].strip().split()
+    x_prev = float(ss[3])
+    y_prev = float(ss[7])
+    z_prev = float(ss[11])
+    ss = t[frameNo].strip().split()
+    x = float(ss[3])
+    y = float(ss[7])
+    z = float(ss[11])
+    return np.sqrt((x - x_prev)*(x - x_prev) + (y - y_prev)*(y - y_prev) + (z - z_prev)*(z - z_prev)), x, y, z
     
 if __name__ == "__main__":
     main()
